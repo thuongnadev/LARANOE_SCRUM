77e87fb2498771068bcc89907a395387645bda87
trietnmpc06220 <monsaidehachiman@gmail.com>
2024-07-26 06:57:42 +0700

    Update button_create.blade.php

--- resources/views/config/admin/data-table/toolbar/button_create.blade.php
+++ resources/views/config/admin/data-table/toolbar/button_create.blade.php
@@ -1,4 +1,1 @@
-<button type="button" class="btn btn-primary" data-bs-toggle="tooltip" title="Coming Soon">
-    <i class="ki-duotone ki-plus fs-2"></i>
-    Add Customer
-</button>
+@livewire("project")
