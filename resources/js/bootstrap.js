69b9e46631cb1eefea7d702cc9935ed615469387
trietnmpc06220 <monsaidehachiman@gmail.com>
2024-07-28 09:30:31 +0700

    Update resource\js\bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
 import { Calendar } from '@fullcalendar/core';
+import 'flowbite';
 import interactionPlugin, { Draggable } from '@fullcalendar/interaction';
-window.axios = axios;
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 
+
+
+Livewire.start();
+Alpine.start();
+
+window.Alpine = Alpine;
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
