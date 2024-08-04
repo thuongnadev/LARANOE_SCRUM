8bea7c827f2792a6f0d2f0c5a61a633ca721a959
trietnmpc06220 <monsaidehachiman@gmail.com>
2024-08-04 19:29:02 +0700

    bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
-import { Calendar } from '@fullcalendar/core';
-import interactionPlugin, { Draggable } from '@fullcalendar/interaction';
-window.axios = axios;
+import 'preline';
+import 'flowbite';
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+import sort from '@alpinejs/sort';
 
+Alpine.plugin(sort);
+Alpine.start();
+Livewire.start();
+
+
+window.Alpine = Alpine;
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
